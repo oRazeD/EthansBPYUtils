8975466ee8143fdcebdd3d961fda0ef373c41138
ethan <ethan.simon.3d@gmail.com>
2022-09-06 02:16:00 -0400

    tweak BMeshFromEditMode

--- utils/bmesh.py
+++ utils/bmesh.py
@@ -3,26 +3,21 @@ import bpy, bmesh
 
 class BMeshFromEditMode():
     '''Generate a temporary BMesh. Accepts Objects or Meshes as inputs temp change'''
-    def __init__(self, bm_data: bpy.types.Object | bpy.types.Mesh):
-        if bpy.context.mode != 'EDIT_MESH':
-            raise ValueError("Object not in Edit Mode")
-        elif bm_data is None:
-            raise ValueError("Object is None")
-
-        if isinstance(bm_data, bpy.types.Object):
-            self.bm_data = bm_data.data
-        elif isinstance(bm_data, bpy.types.Mesh):
-            self.bm_data = bm_data
+    def __init__(self, input_data: bpy.types.Object | bpy.types.Mesh, update_mesh: bool=True):
+        if isinstance(input_data, bpy.types.Mesh):
+            self.input_data = input_data
         else:
-            raise ValueError("Object is the incorrect data type")
+            self.input_data = input_data.data
+
+        self.update_mesh = update_mesh
 
     def __enter__(self):
-        self.bm = bmesh.from_edit_mesh(self.bm_data)
+        self.bm = bmesh.from_edit_mesh(self.input_data)
         return self
      
-    def __exit__(self, _exc_type, _exc_value, _exc_traceback):
-        bmesh.update_edit_mesh(self.bm_data)
-        #self.bm.free() # Causes crashes? Edit Mode uses the same BMesh regardless, free unnecessary
+    def __exit__(self, _exc_type, _exc_value, _exc_traceback): # NOTE do not use self.bm.free() for BMeshes made in Edit Mode, uses same data regardless
+        if self.update_mesh:
+            bmesh.update_edit_mesh(self.input_data)
 
 
 # ##### BEGIN GPL LICENSE BLOCK #####
